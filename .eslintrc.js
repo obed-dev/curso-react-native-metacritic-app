120f03c7ae783ca064dc0715075e54188ea48ada
obed-dev <obedbaltodano23@hotmail.com>
2024-10-24 21:54:01 -0600

    new repo react native app

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+const { rules } = require("eslint-plugin-prettier");
+
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ['expo' , 'prettier'],
+  plugins: ['prettier'],
+  rules: { 
+    'prettier/prettier' : 'error'
+  }
+};
